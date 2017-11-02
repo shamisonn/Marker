e4cffb6727f053ba5f1eadbcca3634d077ac5bfc
shamisonn <unknown0incident@gmail.com>
2017-11-02 19:47:45 +0900

    fix params and remove render

--- src/main/java/com/zzlab/Main.java
+++ src/main/java/com/zzlab/Main.java
@@ -2,25 +2,17 @@ package com.zzlab;
 
 import com.zzlab.repoImpl.MarkerRepoImpl;
 import com.zzlab.services.MarkerService;
-import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
-
-import java.util.Map;
 
 import static spark.Spark.path;
 
 public class Main {
 
     public static void main(String[] args) {
+        String dbPath = "sqlite.db";
         String dbUser = "hoge";
         String dbPass = "fuga";
-        String dbPath = "sqlite.db";
 
-        path("/markers", () -> new MarkerService(new MarkerRepoImpl(dbUser, dbPass, dbPath)));
+        path("/markers", () -> new MarkerService(new MarkerRepoImpl(dbPath, dbUser, dbPass)));
 
-    }
-
-    public static String render(Map<String, Object> model, String templatePath) {
-        return new VelocityTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 }
