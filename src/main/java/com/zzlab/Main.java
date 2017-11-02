3ee11f642b734bfffcbb97a6d416ce0db236f4e9
shamisonn <unknown0incident@gmail.com>
2017-11-03 06:50:59 +0900

    add; /loginにてtoken発行を行うように実装

--- src/main/java/com/zzlab/Main.java
+++ src/main/java/com/zzlab/Main.java
@@ -2,6 +2,7 @@ package com.zzlab;
 
 import com.zzlab.jwt.LoginManager;
 import com.zzlab.repoImpl.MarkerRepoImpl;
+import com.zzlab.repositories.MarkerRepo;
 import com.zzlab.services.LoginService;
 import com.zzlab.services.MarkerService;
 
@@ -16,9 +17,9 @@ public class Main {
 
         LoginManager manager = new LoginManager("aa");
         manager.scheduledCleanBlackList();
-
-        path("/markers", () -> new MarkerService(new MarkerRepoImpl(dbPath, dbUser, dbPass)));
-        path("/login", () -> new LoginService());
+        MarkerRepo markerRepo = new MarkerRepoImpl(dbPath, dbUser, dbPass);
+        path("/markers", () -> new MarkerService(markerRepo));
+        path("/login", () -> new LoginService(markerRepo, manager));
 
     }
 }
